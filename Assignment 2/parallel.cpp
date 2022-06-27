3fe52b8db1cf721eff1604e1e735371fc103e3d4
adams-russell <89750070+adams-russell@users.noreply.github.com>
2022-06-26 19:29:43 -0500

    Create parallel.cpp
    Parallelism-oriented vector dot product

---
+++ Assignment 2/parallel.cpp
@@ -0,0 +1,63 @@
+#include "omp.h"
+#include <iostream>
+#include <vector>
+
+int i, total, size, angle;
+std::vector<int> vec1, vec2, product;
+
+int vecMultiply(std::vector<int>& vec1, std::vector<int>& vec2, std::vector<int>& product, int total, int angle, int size);
+void fillVec(std::vector<int>& vec, int size);
+
+int main() {
+
+	//Get angle
+	std::cout << "Input vector angle: " << std::endl;
+	std::cin >> angle;
+
+
+	//Get size for vectors, which must be the same
+	std::cout << "How many items in each vector?" << std::endl;
+	std::cin >> size;
+	vec1.resize(size);
+	vec2.resize(size);
+	product.resize(size);
+
+	//Fill each vector
+	fillVec(vec1, size);
+	fillVec(vec2, size);
+
+	//Multiply vectors
+	std::cout << vecMultiply(vec1, vec2, product, total, angle, size);
+
+	system("pause");
+	return 0;
+}
+
+int vecMultiply(std::vector<int>& vec1, std::vector<int>& vec2, std::vector<int>& product, int total, int angle, int size) {
+	omp_lock_t writelock;
+	omp_init_lock(&writelock);
+
+	#pragma omp parallel for shared(size)
+	for (i = 0; i < size; i++) {
+		omp_set_lock(&writelock);
+		product.at(i) = vec1.at(i) * vec2.at(i);
+		omp_unset_lock(&writelock);
+	}
+	omp_destroy_lock(&writelock);
+
+	#pragma omp parallel for shared(size)
+	for (i = 0; i < size; i++) {
+	 #pragma omp atomic
+		std::cout << product[i];
+		total += product.at(i);
+	}
+	return total * cos(angle);
+}
+
+void fillVec(std::vector<int>& vec, int size) {
+	int num;
+	for (i = 0; i < size; i++) {
+		std::cin >> num;
+		vec.at(i) = num;
+	}
+}
